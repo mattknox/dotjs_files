95fae88503abf371acb73a6e4b0028a17653d167
matt knox <matthewknox@gmail.com>
2011-10-21 15:12:07 -0700

    added a TODO.

---
+++ default.js
@@ -0,0 +1,9 @@
+// var fl = document.forms;
+
+// fl.filter ;
+
+// function likelysignup(f) {
+//   var ret = f.action.match(/(register)|(signup)/);
+//   ret = ret || f;
+// }
+// TODO: insert a hook so that I can have rescuetime-like tracking functionality 
