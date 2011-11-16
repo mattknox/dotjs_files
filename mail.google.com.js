bf236f78cc869bd5ccba0dd456792363304e5c7b
matt knox <matthewknox@gmail.com>
2011-11-16 10:51:01 -0800

    changes to mail.google.com.js...need to make this work on all goog domains.

--- mail.google.com.js
+++ mail.google.com.js
@@ -1,1 +1,10 @@
-$("gbg1").hide();
+function knoxPostLoad() {
+//  alert("hello");
+  $("#gbg1").parent().innerHTML = "foo"; //.css("display", "none");
+//  alert($("a#gbg1").length);
+//  alert($("#gbg1").length);
+}
+
+window.knoxPostLoad = knoxPostLoad;
+//setTimeout('window.knoxPostLoad()', 6000);
+
