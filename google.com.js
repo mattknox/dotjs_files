d6c2de17d1c533f48c67d550c078ea7143fd379e
matt knox <matthewknox@gmail.com>
2011-11-20 15:16:01 -0800

    more small changes.  Seems to work on google.com now

--- google.com.js
+++ google.com.js
@@ -1,8 +1,8 @@
 function googPostLoad() {
   $("#gbi1").css("visibility", "hidden");
+  $("#gbi1a").css("visibility", "hidden");
   $("#gbgs1").css("visibility", "hidden");
 }
 
 window.googPostLoad = googPostLoad;
 setTimeout('window.googPostLoad()', 6000);
-
