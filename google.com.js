1ba092e8ba0c3a32e66735f48fa1c93a4eacc162
matt knox <matthewknox@gmail.com>
2011-11-26 13:10:47 -0800

    removed spurious comment lines.

--- google.com.js
+++ google.com.js
@@ -1,8 +1,3 @@
-//function googPostLoad() {
-  $("#gbi1").css("visibility", "hidden");
-  $("#gbi1a").css("visibility", "hidden");
-  $("#gbgs1").css("visibility", "hidden");
-//}
-
-//window.googPostLoad = googPostLoad;
-//setTimeout('window.googPostLoad()', 6000);
+$("#gbi1").css("visibility", "hidden");
+$("#gbi1a").css("visibility", "hidden");
+$("#gbgs1").css("visibility", "hidden");
