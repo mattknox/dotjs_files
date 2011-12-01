a3dc040a3157e11a3f87aed2c20278282b80387a
matt knox <matthewknox@gmail.com>
2011-11-30 20:26:02 -0800

    comment

--- google.com.js
+++ google.com.js
@@ -1,3 +1,4 @@
 $("#gbi1").css("visibility", "hidden");
 $("#gbi1a").css("visibility", "hidden");
 $("#gbgs1").css("visibility", "hidden");
+// figure out what to do about the new, post black-bar era.
