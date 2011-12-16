fcef8f2f5f9033b8e47f70fca5a63c9f32a8c38d
matt knox <matthewknox@gmail.com>
2011-12-15 18:22:28 -0800

    fixes to xkcd.com.js

--- xkcd.com.js
+++ xkcd.com.js
@@ -1,8 +1,9 @@
 var comic = $("div.s img[title]");
+var title = $("div.s img[title]")[1].title;
 var titleP = $("<p></p>");
 titleP.css({"font-variant": "normal",
             "border": "1px solid yellow",
             "padding": "1em",
             "background-color": "#FFFFCC"});
-titleP.text(comic.attr("title"));
+titleP.text(title);
 comic.after(titleP);
